f4f194db32b25670458f638539994eb30e2e5d2b
Joerg Henning <henning.joerg@gmail.com>
2014-03-15 12:55:24 +0700

    Updated MySQL package, reverted base box to http://files.vagrantup.com/precise64.box

--- puppet/modules/php/manifests/phpunit.pp
+++ puppet/modules/php/manifests/phpunit.pp
@@ -4,23 +4,27 @@ class php::phpunit {
     require => Class["php::pear"],
     command => "/usr/bin/pear install --alldeps pear.phpunit.de/PHPUnit",
     creates => "/usr/bin/phpunit",
+    returns => [0, '']
   }
 
   exec {"pear install phpunit-selenium":
     require => [Exec["pear install phpunit"], Package["php5-curl"]],
     command => "/usr/bin/pear install --alldeps phpunit/PHPUnit_Selenium",
     creates => "/usr/share/php/PHPUnit/Extensions/Selenium2TestCase/",
+    returns => [0, '']
   }
 
   exec {"pear install phpunit-dbunit":
     require => Exec["pear install phpunit"],
     command => "/usr/bin/pear install --alldeps phpunit/DbUnit",
     creates => "/usr/share/php/PHPUnit/Extensions/Database/",
+    returns => [0, '']
   }
 
   exec {"pear install phpunit-story":
     require => Exec["pear install phpunit"],
     command => "/usr/bin/pear install --alldeps phpunit/PHPUnit_Story",
-    creates => "/usr/share/php/PHPUnit/Extensions/Story/"
+    creates => "/usr/share/php/PHPUnit/Extensions/Story/",
+    returns => [0, '']
   }
 }
