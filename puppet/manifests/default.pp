b8d34dea1f77e0b8db06e395fed059f2c41c231d
Joerg Henning <henning.joerg@gmail.com>
2015-02-11 15:15:04 +0700

    Force apt-get update in init stage
    Fix is a bit hackish, see comment in default.pp

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -2,16 +2,24 @@
 # if size("${apt_mirror}") == 0 { $apt_mirror = "archive.ubuntu.com" }
 if size("${site_name}") == 0 { $site_name = "my_site" }
 
+# FIXME: put this somewhere else (we already have a similar task in apt-mirror
+# but it's refreshonly and won't work when called directly)
+class apt_update {
+  exec { "do_apt_update":
+    command     => "/usr/bin/apt-get update",
+    timeout     => 300,
+  }
+}
+
 # Set up package repos in init stage
-# stage { "init": before  => Stage["main"] }
-# class { "apt-mirror": 
-#   stage => init, 
-#   apt_mirror => $apt_mirror 
-# }
+stage { "init": before  => Stage["main"] }
+class { "apt_update": 
+  stage => init
+}
 
 # LAMPP Stack - Apache2, PHP, Skip MySQL (faster)
 class { "lampp": 
-  mysql => false,
+  mysql => true,
   ppa => true
 }
 
@@ -29,5 +37,5 @@ class { "php::composer": }
 # Site config - Apache vhost, MySQL database
 class { "site": 
   site_name => $site_name,
-  docroot => "/var/www/${site_name}/app"
+  docroot => "/var/www/${site_name}/public"
 }
